e7d926334a3b3f75e1dc73ca6322383adbdafdd8
kasvrol <dovalekarolina@gmail.com>
2022-12-29 16:37:25 -0300

    feat: funções que farão a correspondencia de dados entre componentes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Head from "next/head";
 import Cards from "../components/cards";
 import Header from "../components/header";
+import Menu from "../components/menu";
+import Pages from "../components/pagess";
 import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+    const [page, setPage] = useState(1);
+    const [item, setItem] = useState(0);
+
+    const qtdProjects = (qtd) => {
+        setItem(qtd);
+    };
+
+    const changePage = (page) => {
+        setPage(page);
+    };
+
     return (
         <>
             <Head>
@@ -16,8 +30,12 @@ export default function Home() {
             </Head>
             <Header />
             <main className={styles.main}>
-                <Cards />
+                <Menu />
+                <Cards qtdProjects={qtdProjects} projectsPage={page} />
             </main>
+            <footer className={styles.footer}>
+                <Pages qtdProjects={item} changePage={changePage} />
+            </footer>
         </>
     );
 }
